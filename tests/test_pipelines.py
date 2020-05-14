1c82d93504b2f170d52ffd9149b540b0e826b215
Hemant Pawar <hpawar@dwavesys.com>
2020-05-14 23:26:04 +0000

    Added conditional output

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -32,8 +32,9 @@ class TestDemo(unittest.TestCase):
         demo_file = os.path.join(project_dir, 'pipelines.py')
         output = subprocess.check_output([sys.executable, demo_file])
         output = str(output).upper()
-        print("Example output \n"+ output)
-
+        if os.getenv('DEBUG_OUTPUT'):
+            print("Example output \n"+ output)
+            
         with self.subTest(msg="Verify if output contains 'Minimum vertex cover found' \n"):
             self.assertIn("Minimum vertex cover found".upper(), output)
         with self.subTest(msg="Verify if error string contains in output \n"):
